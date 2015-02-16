68f137a19dae18dca219a3c13c404a010a38b9f8
Daniel Chiu <daniechiu@gmail.com>
2015-02-16 10:42:15 -0800

    Adds code to compute area of polygons without sides that cross over each other.

--- paddocks.js
+++ paddocks.js
@@ -1,8 +1,24 @@
 // References:
 // * http://www.mathopenref.com/polygonirregulararea.html
 // * http://www.mathopenref.com/coordtrianglearea.html
+// * http://www.mathopenref.com/coordpolygonarea.html
 
-function areaOfTriangle(p1, p2, p3) {
-  return Math.abs((p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y))/2);
+function areaOfPolygon(points) {
+  var numPoints = points.length;
+  var area = 0;
+  for (var i = 0; i < numPoints; i++) {
+    p1 = points[i];
+    p2 = points[0];
+    if (i < numPoints - 1) {
+      p2 = points[i+1];
+    }
+    area += p1.x * p2.y - p1.y * p2.x;
+  }
+  area /= 2;
+  area = Math.abs(area);
+  return area;
 }
 
+// TODO: File input
+var testCase1 = [{x: 4, y: 6}, {x: 4, y: -4}, {x: 8, y: -4}, {x: 8, y: -8}, {x: -4, y: -8}, {x: -4, y: 6}];
+console.log(areaOfPolygon(testCase1));
