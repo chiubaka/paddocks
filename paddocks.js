60ccad91731bc8fcc315f82058e4218ad9e4efc9
Daniel Chiu <daniechiu@gmail.com>
2015-02-16 10:21:21 -0800

    Adds function to compute area of triangle from 3 arbitrary points.

--- paddocks.js
+++ paddocks.js
@@ -1,1 +1,8 @@
-console.log("Test")
+// References:
+// * http://www.mathopenref.com/polygonirregulararea.html
+// * http://www.mathopenref.com/coordtrianglearea.html
+
+function areaOfTriangle(p1, p2, p3) {
+  return Math.abs((p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y))/2);
+}
+
